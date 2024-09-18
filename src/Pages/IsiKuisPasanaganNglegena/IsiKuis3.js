5fbb5ddb562b75adbd475d2adf9e4adbc8f2d8cd
nabnabila <nabila.indhy@gmail.com>
2024-09-18 19:19:25 +0700

    update

---
+++ src/Pages/IsiKuisPasanaganNglegena/IsiKuis3.js
@@ -0,0 +1,31 @@
+import "../../App.css";
+import "../../style/Crossword.css";
+import { Link } from "react-router-dom";
+import NavigationBar from "../../components/NavigationBar";
+import UtakAtikGathukPasanganNglegena from "../../components/isikuispasangannglegena/UtakAtikGathukPasanganNglegena";
+function IsiKuis3B() {
+  return (
+    <div>
+      {/* Navigation Bar Container */}
+      <div className="navbar-container">
+        {/* Back Arrow */}
+        <Link to="/pasanganaksaranglegena/kuis2" className="back-arrow-navbar">
+          <i className="bi bi-arrow-left"></i>
+        </Link>
+
+        {/* Navigation Bar */}
+        <div className="navbar-center">
+          <NavigationBar />
+        </div>
+      </div>
+
+      {/* mode */}
+      <div>
+        <UtakAtikGathukPasanganNglegena />
+      </div>
+      {/* mode */}
+    </div>
+  );
+}
+
+export default IsiKuis3B;
